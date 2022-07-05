c1d9702db061fb3bcd352dcc3bf338b688f3dde4
VictorSCR <104372813+VictorSCR@users.noreply.github.com>
2022-07-05 22:28:17 +0500

    add all files

--- homework_01/main.py
+++ homework_01/main.py
@@ -35,11 +35,15 @@ def filter_numbers(numbers_list, filter_type):
         return [number for number in numbers_list if number % 2 != 0]
     if filter_type == EVEN:
         return [number for number in numbers_list if number % 2 == 0]
+    if filter_type == PRIME:
+        return list( filter(is_prime, numbers_list))
 
-def isPrime(n):
-    if n % 2 == 0:
-        return n == 2
-    d = 3
-    while d * d <= n and n % d != 0:
-        d += 2
-    return d * d > n
+def is_prime(n):
+    if n < 2:
+        return False
+    if n == 2:
+        return True
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
